99fc11c3c3739075cb20abf374aef9a61ba6eb65
Daniel Oloruntoba <danieloloruntoba681@gmail.com>
2023-03-09 02:35:06 +0100

    socket connection successful

--- index.js
+++ index.js
@@ -5,13 +5,25 @@ const http = require("http");
 const server = http.createServer(app);
 require("dotenv").config();
 const { Server } = require("socket.io");
-const io = new Server(server);
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT"],
+  },
+});
 
 //Middleware
 app.use(cors());
 
 io.on("connection", (socket) => {
-  console.log("user connected");
+  console.log(`user connected, id: ${socket.id}`);
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log(`User with id ${socket.id} has joined room ${data}`);
+  });
+  socket.on("disconnect", () => {
+    console.log(`User disconnected, id: ${socket.id}`);
+  });
 });
 
 server.listen(process.env.PORT || 4000, () => {
